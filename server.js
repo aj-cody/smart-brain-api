cb3fc70bc4930a386440ebe52649610c71db5507
Archit Jain <archit.abj@gmail.com>
2020-10-09 06:45:59 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+//controllers
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const imageCount = require('./controllers/imageCount');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: 'localhost',
+        user: 'postgres',
+        password: 'test',
+        database: 'smart-brain'
+    }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+//signin
+app.post('/signin', signin.handleSignIn(db, bcrypt))
+
+//register
+app.post('/register', register.handleRegister(db, bcrypt))
+
+//profile_user
+app.get('/profile/:id', profile.handleProfileGet(db))
+
+//image count
+app.put('/image', imageCount.handleImageCount(db))
+
+//clarifai
+app.post('/imageurl', (req, res) => { imageCount.handleApiCall(req, res)})
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+})
